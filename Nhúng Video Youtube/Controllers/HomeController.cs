8cc92d122df1dee596c7c074425a8eb80ef1dfd7
Windows <vtnduc2382000@gmail.com>
2023-12-03 13:36:54 +0700

    Add project files.

---
+++ Nhúng Video Youtube/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nhúng_Video_Youtube.Models;
+using System.Diagnostics;
+
+namespace Nhúng_Video_Youtube.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View("About");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Nhúng Video Youtube/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nhúng_Video_Youtube.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
